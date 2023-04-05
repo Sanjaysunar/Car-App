3afe270ac96492e754765b22e0e04143095c796b
Sanjay Sunar <sansunar11@gmail.com>
2023-04-06 03:10:38 +0530

    Owner and its CRUD repository creation

---
+++ src/main/java/com/sanjay/cardatabase/domain/Owner.java
@@ -0,0 +1,45 @@
+package com.sanjay.cardatabase.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Owner {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long ownerid;
+    private String firstname, lastname;
+
+    public Owner(){}
+
+    public Owner(String firstname, String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public Long getOwnerid() {
+        return ownerid;
+    }
+
+    public void setOwnerid(Long ownerid) {
+        this.ownerid = ownerid;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
