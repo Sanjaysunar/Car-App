7b727042fa6d9113fb4968b563eab26597f8be4b
Sanjay Sunar <sansunar11@gmail.com>
2023-04-05 14:40:07 +0530

    Initial commit

---
+++ src/test/java/com/sanjay/cardatabase/CarAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanjay.cardatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
