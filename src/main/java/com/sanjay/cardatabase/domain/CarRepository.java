89f84bb270f0aeae4d8bd596a74400c2cc1aa1b8
Sanjay Sunar <sansunar11@gmail.com>
2023-04-06 20:08:34 +0530

    Used Spring DATA REST for RESTful service

--- src/main/java/com/sanjay/cardatabase/domain/CarRepository.java
+++ src/main/java/com/sanjay/cardatabase/domain/CarRepository.java
@@ -1,6 +1,7 @@
 package com.sanjay.cardatabase.domain;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 public interface CarRepository extends CrudRepository<Car, Long> {
 }
