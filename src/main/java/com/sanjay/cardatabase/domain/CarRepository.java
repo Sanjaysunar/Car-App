66a97bc683fcb7fd982f6cd484ad1c24df074f41
Sanjay Sunar <sansunar11@gmail.com>
2023-04-16 23:23:03 +0530

    spring security userDetails

--- src/main/java/com/sanjay/cardatabase/domain/CarRepository.java
+++ src/main/java/com/sanjay/cardatabase/domain/CarRepository.java
@@ -1,7 +1,6 @@
 package com.sanjay.cardatabase.domain;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 public interface CarRepository extends CrudRepository<Car, Long> {
 }
