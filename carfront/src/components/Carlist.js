e7bfc276147fd6306b52592a5a4ec483738ab38d
Sanjay Sunar <sansunar11@gmail.com>
2023-05-18 23:04:46 +0530

    Delete functionality added
    - onDelClick for delete

--- carfront/src/components/Carlist.js
+++ carfront/src/components/Carlist.js
@@ -1,23 +1,55 @@
 import React, { useEffect, useState } from "react";
 import { SERVER_URL } from "../constants";
 import { DataGrid } from "@mui/x-data-grid";
+import { Snackbar } from "@mui/material";
 
 function Carlist() {
     const [cars, setCars] = useState([]);
+    const [open, setOpen] = useState(false);
     
     useEffect(() => {
+        fetchCars();
+    }, []);
+
+    const fetchCars = () => {
         fetch(SERVER_URL + "api/cars")
         .then(response => response.json())
         .then(data => setCars(data._embedded.cars))
         .catch(err => console.error(err));
-    }, []);
+    }
+
+    const onDelClick = (url) => {
+        if (window.confirm("Are you sure to delete?")){
+            fetch(url, {method: 'DELETE'})
+            .then(response => {
+                if (response.ok){
+                    fetchCars();
+                    setOpen(true);
+                }
+                else {
+                    alert('Something went wrong!');
+                }
+            })
+            .catch(err => console.error(err))
+        }
+    }
 
     const columns = [
         {field: 'brand', headerName: 'Brand', width: 200},
         {field: 'model', headerName: 'Model', width: 200},
         {field: 'color', headerName: 'Color', width: 200},
         {field: 'year', headerName: 'Year', width: 150},
-        {field: 'price', headerName: 'Price', width: 150}
+        {field: 'price', headerName: 'Price', width: 150},
+        {
+            field: '_links.self.href',
+            headerName: '',
+            sortable: false,
+            filterable: false,
+            renderCell: row =>
+            <button
+                onClick={() => onDelClick(row.id)}>Delete
+            </button>
+        }
     ];
 
     return(
@@ -25,7 +57,16 @@ function Carlist() {
             <DataGrid
                 rows={cars}
                 columns={columns}
-                getRowId={row => row._links.self.href}/>
+                disableSelectionOnClick={true}
+                getRowId={row => row._links.self.href}
+            />
+            <Snackbar
+                open={open}
+                autoHideDuration={2000}
+                onClose={() => setOpen(false)}
+                message="Car deleted"
+            />
+
                 </div>
             );
 }
