9e1ab76d58d8216ce26bebb760fe45f4d4caaca8
Sanjay Sunar <sansunar11@gmail.com>
2023-06-02 02:35:43 +0530

    Update Carlist.js
    - Added export into csv and print functionality

--- carfront/src/components/Carlist.js
+++ carfront/src/components/Carlist.js
@@ -1,9 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { SERVER_URL } from "../constants";
-import { DataGrid } from "@mui/x-data-grid";
+import { DataGrid, GridToolbarContainer,GridToolbarExport, gridClasses } from "@mui/x-data-grid";
 import { Snackbar } from "@mui/material";
 import AddCar from "./AddCar";
 import EditCar from "./EditCar";
+
+function CustomToolbar() {
+    return (
+        <GridToolbarContainer
+            className={gridClasses.toolbarContainer}>
+            <GridToolbarExport />
+        </GridToolbarContainer>
+    );
+}
 
 function Carlist() {
     const [cars, setCars] = useState([]);
@@ -113,6 +122,7 @@ function Carlist() {
                     columns={columns}
                     disableSelectionOnClick={true}
                     getRowId={row => row._links.self.href}
+                    components={{Toolbar: CustomToolbar}}
                 />
                 <Snackbar
                     open={open}
