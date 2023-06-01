78b090973a66d1b0e5c35ffd4f7440ebaae2eedd
Sanjay Sunar <sansunar11@gmail.com>
2023-06-02 01:40:42 +0530

    Update Carlist.js

--- carfront/src/components/Carlist.js
+++ carfront/src/components/Carlist.js
@@ -64,6 +64,16 @@ function Carlist() {
         {field: 'year', headerName: 'Year', width: 150},
         {field: 'price', headerName: 'Price', width: 150},
         {
+            field: '_links.car.href',
+            headerName: '',
+            sortable: false,
+            filterable: false,
+            renderCell: row =>
+                <EditCar 
+                    data={row}
+                    updateCar={updateCar} />
+        },
+        {
             field: '_links.self.href',
             headerName: '',
             sortable: false,
@@ -93,35 +103,6 @@ function Carlist() {
             })
             .catch(err => console.error(err))
     }
-
-    const columns = [
-        {field: 'brand', headerName: 'Brand', width: 200},
-        {field: 'model', headerName: 'Model', width: 200},
-        {field: 'color', headerName: 'Color', width: 200},
-        {field: 'year', headerName: 'Year', width: 150},
-        {field: 'price', headerName: 'Price', width: 150},
-        {
-            field: '_links.car.href',
-            headerName: '',
-            sortable: false,
-            filterable: false,
-            renderCell: row =>
-                <EditCar 
-                    data={row}
-                    updateCar={updateCar} />
-        },
-        {
-            field: '_links.self.href',
-            headerName: '',
-            sortable: false,
-            filterable: false,
-            renderCell: row =>
-                <button 
-                    onClick={() => onDelClick{row.id}}>
-                    Delete
-                </button>
-        }
-    ];
 
     return(
         <React.Fragment>
