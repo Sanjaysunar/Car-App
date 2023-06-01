30c14db321bded5799bb1d9370fe9103dab56a8c
Sanjay Sunar <sansunar11@gmail.com>
2023-06-02 01:34:04 +0530

    Update Carlist.js
    - added PUT method in updateCar with content type & two argument - car object and request URL
    - imported EditCar component into Carlist component
    - added EditCar component to the table columns and Edit button
    - two props passed in EditCar component: row & updateCar function

--- carfront/src/components/Carlist.js
+++ carfront/src/components/Carlist.js
@@ -3,6 +3,7 @@ import { SERVER_URL } from "../constants";
 import { DataGrid } from "@mui/x-data-grid";
 import { Snackbar } from "@mui/material";
 import AddCar from "./AddCar";
+import EditCar from "./EditCar";
 
 function Carlist() {
     const [cars, setCars] = useState([]);
@@ -71,6 +72,54 @@ function Carlist() {
             <button
                 onClick={() => onDelClick(row.id)}>Delete
             </button>
+        }
+    ];
+
+    //Update Car
+    const updateCar = (car,link) => {
+        fetch(link,
+            {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(car)
+            })
+            .then(response => {
+                if (response.ok) {
+                    fetchCars();
+                }
+                else {
+                    alert('Something went wrong!');
+                }
+            })
+            .catch(err => console.error(err))
+    }
+
+    const columns = [
+        {field: 'brand', headerName: 'Brand', width: 200},
+        {field: 'model', headerName: 'Model', width: 200},
+        {field: 'color', headerName: 'Color', width: 200},
+        {field: 'year', headerName: 'Year', width: 150},
+        {field: 'price', headerName: 'Price', width: 150},
+        {
+            field: '_links.car.href',
+            headerName: '',
+            sortable: false,
+            filterable: false,
+            renderCell: row =>
+                <EditCar 
+                    data={row}
+                    updateCar={updateCar} />
+        },
+        {
+            field: '_links.self.href',
+            headerName: '',
+            sortable: false,
+            filterable: false,
+            renderCell: row =>
+                <button 
+                    onClick={() => onDelClick{row.id}}>
+                    Delete
+                </button>
         }
     ];
 
